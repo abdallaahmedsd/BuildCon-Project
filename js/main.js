343051a34d42700cebcfa79f36e183f5a01d38b2
abdallaahmedsd <abdallaahmed172125@gmail.com>
2023-07-09 00:01:23 +0300

    The Project

---
+++ js/main.js
@@ -0,0 +1,41 @@
+const links = document.querySelectorAll('.navbar-nav .nav-link');
+
+links.forEach(link => {
+  link.addEventListener('click', () => {
+    links.forEach(link => {
+      link.classList.remove('active');
+    });
+    link.classList.add('active');
+    document.querySelector('.navbar-collapse').classList.remove("show");
+  });
+});
+
+document.body.addEventListener('click', () => {
+  document.querySelector('.navbar-collapse').classList.remove("show");
+});
+
+document.querySelector('.navbar-brand').onclick = function () {
+  links.forEach(link => {
+    link.classList.remove('active');
+    if (link.classList.contains('home')) {
+      link.classList.add('active');
+    }
+  });
+}
+
+const goUp = document.querySelector('.go-up');
+
+window.onscroll = function () {
+  if (this.scrollY >= 1000) {
+    goUp.style.right = '20px';
+  } else {
+    goUp.style.right = '-60px';
+  }
+}
+
+goUp.onclick = function () {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth',
+  });
+}
